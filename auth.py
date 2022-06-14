3d8b02be7c9ca4e75e0c5318d91ccdf31f9b0f82
mohamad nosratian <68176509+mohamadnosratian@users.noreply.github.com>
2022-06-14 21:19:21 +0430

    Initialize from cookiecutter

---
+++ auth.py
@@ -0,0 +1,133 @@
+from datetime import datetime, timedelta
+from typing import Union
+from fastapi import Depends, FastAPI, HTTPException, status, Body
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from jose import JWTError, jwt
+from passlib.context import CryptContext
+from pydantic import BaseModel
+from models.base_models import UpdateUser, User, ReturnUser, RegisterUser, LoginUser
+from tools.useful import (
+    valitdatePassword,
+    validateUsername,
+    create_user,
+    verify_password,
+)
+import motor.motor_asyncio
+import os
+import dotenv
+from fastapi.encoders import jsonable_encoder
+import time
+
+# to get a string like this run:
+# openssl rand -hex 32
+SECRET_KEY = "543db4cafaabcfe8a7357f7821faf2cced1a90909cf65fb947f2d29f080be897"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+
+def create_access_token(data: dict, expires_delta: Union[timedelta, None] = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.utcnow() + expires_delta
+    else:
+        expire = datetime.utcnow() + timedelta(minutes=15)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+
+async def loggin(pwd_context: CryptContext, user: LoginUser, type: str):
+    if type == "username":
+        user_instant = await db["users"].find_one({type: user.username})
+    else:
+        user_instant = await db["users"].find_one({type: user.email})
+    if user_instant is None:
+        raise HTTPException(400, "User not found!")
+    user_instant = User.parse_obj(user_instant)
+    verify_password(pwd_context, user.password, user_instant.password)
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_access_token(
+        data={"sub": user_instant.username}, expires_delta=access_token_expires
+    )
+    user_instant.token = access_token
+    update_user = jsonable_encoder(user_instant)
+    result = await db["users"].update_one(
+        {"username": user_instant.username}, {"$set": update_user}
+    )
+    if result is None:
+        raise HTTPException(500, "some thing happend!")
+    print(result)
+    return ReturnUser.parse_obj(user_instant)
+
+
+dotenv.load_dotenv()
+client = motor.motor_asyncio.AsyncIOMotorClient(os.getenv("MONGODB_URL"))
+db = client["auth"]
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+
+app = FastAPI()
+
+
+@app.post("/register", response_model=ReturnUser)
+async def register(user: RegisterUser = Body()):
+    if user.username is None and user.email is None:
+        raise HTTPException(400, "username or email must be included!")
+    valitdatePassword(user.password)
+    user_instant_username = None
+    user_instant_email = None
+    if user.username is not None:
+        validateUsername(user.username)
+        user_instant_username = await db["users"].find_one({"username": user.username})
+    if user.email is not None:
+        user_instant_email = await db["users"].find_one({"email": user.email})
+    if user_instant_username or user_instant_email:
+        raise HTTPException(400, "This user is already taken!")
+    return await create_user(client, RegisterUser.parse_obj(user), pwd_context)
+
+
+class Token(BaseModel):
+    token: str
+
+
+@app.post("/token/login", response_model=ReturnUser)
+async def token_login(token: Token = Body()):
+    if token.token is None:
+        raise HTTPException(400, "token should be specified!")
+    try:
+        payload = jwt.decode(token.token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get("sub")
+        expire: int = payload.get("exp")
+        if username is None or expire is None:
+            raise HTTPException(401, "Could not validate credentials")
+    except JWTError:
+        raise HTTPException(401, "Could not validate credentials")
+    if expire - time.time() <= 0:
+        raise HTTPException(401, "Could not validate credentials")
+    user_instant = await db["users"].find_one({"username": username})
+    if user_instant is None:
+        raise HTTPException(400, "User not found!")
+    user_instant = User.parse_obj(user_instant)
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_access_token(
+        data={"sub": user_instant.username}, expires_delta=access_token_expires
+    )
+    user_instant.token = access_token
+    update_user = jsonable_encoder(user_instant)
+    await db["users"].update_one(
+        {"username": user_instant.username}, {"$set": update_user}
+    )
+    return ReturnUser.parse_obj(user_instant)
+
+
+@app.post("/login", response_model=ReturnUser)
+async def login(user: LoginUser = Body()):
+    if user.username is None and user.email is None:
+        raise HTTPException(400, "username or email must be included!")
+    if user.username:
+        validateUsername(user.username)
+        return await loggin(pwd_context, user, "username")
+    if user.email:
+        return await loggin(pwd_context, user, "email")
---
+++ tools/useful.py
@@ -0,0 +1,57 @@
+import motor.motor_asyncio
+from models.base_models import User, ReturnUser, RegisterUser, LoginUser
+from passlib.context import CryptContext
+from fastapi import HTTPException, status
+import re
+from datetime import datetime, timedelta
+from fastapi.encoders import jsonable_encoder
+
+
+async def create_user(
+    client: motor.motor_asyncio.AsyncIOMotorClient,
+    user: RegisterUser,
+    pwd_context: CryptContext,
+) -> ReturnUser:
+    db = client["auth"]
+    created_user = User.parse_obj(user)
+    created_user.password = get_password_hash(pwd_context, created_user.password)
+    created_user = jsonable_encoder(created_user)
+    res = await db["users"].insert_one(created_user)
+    new_user = await db["users"].find_one({"_id": res.inserted_id})
+    if new_user:
+        return ReturnUser.parse_obj(new_user)
+    return ReturnUser({})
+
+
+def verify_password(pwd_context: CryptContext, plain_password, hashed_password):
+    return pwd_context.verify(plain_password, hashed_password)
+
+
+def get_password_hash(pwd_context: CryptContext, password):
+    return pwd_context.hash(password)
+
+
+def valitdatePassword(passtext: str):
+    if len(passtext) < 6 or len(passtext) > 30:
+        raise HTTPException(400, "Password must be between 6 to 30 character!")
+    re.findall
+    res = re.findall(r"[a-z]+", passtext)
+    if len(res) <= 0:
+        raise HTTPException(400, "Password must include chars, symbols and numbers")
+    res = re.findall(r"[A-Z]+", passtext)
+    if len(res) <= 0:
+        raise HTTPException(400, "Password must include chars, symbols and numbers")
+    res = re.findall(r"[(){}[\]|`¬¦! \"£$%^&*\"<>:;#~_\-+=,@]+", passtext)
+    if len(res) <= 0:
+        raise HTTPException(400, "Password must include chars, symbols and numbers")
+    res = re.findall(r"[0-9]+", passtext)
+    if len(res) <= 0:
+        raise HTTPException(400, "Password must include chars, symbols and numbers")
+
+
+def validateUsername(username: str):
+    res = re.findall(r"[&=_\'-+,<>]+|\.{2,}", username)
+    if len(res) > 0:
+        raise HTTPException(
+            400, "Username most not include (&=_'-+,<>) and or more than one '.'!"
+        )
